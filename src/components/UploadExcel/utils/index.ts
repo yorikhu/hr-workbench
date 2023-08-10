87e9a63bed59f4c6575e227f69e3c65bfd3ecaba
yorikhu <shallowink1999@163.com>
2023-08-10 22:19:33 +0800

    feat: import xlsx to read excel file

--- src/components/UploadExcel/utils/index.ts
+++ src/components/UploadExcel/utils/index.ts
@@ -1,15 +1,32 @@
-import { FileType } from "../types";
+import { read } from "xlsx";
 
 export const formateFileInfo = (file: File | undefined) => {
   if (!file) return {};
-
-  const rawName = file.name;
+  const { name, size, type, lastModified } = file;
+  const rawName = name;
   const rawNameChip = rawName?.split(".");
   const extension = rawNameChip[rawNameChip.length - 1];
   const fileName = rawNameChip.slice(0, rawNameChip.length - 1).join(".");
 
+  const fileReader = new FileReader();
+  fileReader.readAsBinaryString(file);
+  fileReader.onload = function (e) {
+    try {
+      let data = e.target?.result;
+      let workbook = read(data, {
+        type: "binary",
+      });
+      console.log(workbook);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return {
     name: fileName,
     extension: extension,
+    size,
+    type,
+    lastModified,
   };
 };
