2967c70c2290610808703618bbf52e1810e26f78
yorikhu <yorikhu@gmail.com>
2026-08-06 22:14:30 +0800

    chore: ignore warning

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        silenceDeprecations: ['legacy-js-api', 'import']
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
